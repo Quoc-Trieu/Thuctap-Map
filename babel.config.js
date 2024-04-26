6902c5fef668c6bba998bcbece508d3f6a21dcb7
Desplay Shido <101574256+Desplay@users.noreply.github.com>
2024-04-26 15:59:19 +0700

    update dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: {
+            "@components": "./src/components",
+            "@features": "./src/features",
+            "@data": "./src/data",
+            "@assets": "./assets",
+            "@utils": "./src/utils",
+            "@screens": "./src/screens",
+            "@navigations": "./src/navigations",
+            "@assets": "./src/assets",
+            "@services": "./src/services",
+          },
+        },
+      ],
+    ],
+  };
 };
