9c6530448e3154f81bf715831f3b52a45f26ce29
Desplay Shido <101574256+Desplay@users.noreply.github.com>
2024-05-02 09:07:47 +0700

    update services

--- src/utils/coverageToGeoJSON.ts
+++ src/utils/coverageToGeoJSON.ts
@@ -1,4 +1,7 @@
-export default function coverageToGeoJSON(input: any) {
+export function coverageToGeoJSON(input: any) {
+  if (!input.routes || !input.routes.length) {
+    return null;
+  }
   const Geometry = {
     type: 'FeatureCollection',
     features: [
@@ -12,8 +15,25 @@ export default function coverageToGeoJSON(input: any) {
       },
     ],
   };
-  let coordinates = input['routes'][0]['geometry']['coordinates'];
+  let coordinates = input.routes[0].geometry.coordinates;
   Geometry.features[0].geometry.coordinates = coordinates;
 
+  return Geometry;
+}
+
+export function getCoodinatesFromResponse(input: any) {
+  const Geometry = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        properties: {},
+        geometry: {},
+      },
+    ],
+  };
+  const {paths} = input;
+  const {points} = paths[0];
+  Geometry.features[0].geometry = points;
   return Geometry;
 }
---
+++ src/utils/handleData.ts
@@ -0,0 +1,33 @@
+export function msToTime(ms: any) {
+  let seconds = ms / 1000;
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  minutes = minutes % 60;
+  hours = hours % 24;
+
+  minutes = Math.round(minutes * 10) / 10;
+
+  let timeString = '';
+  if (hours > 0) {
+    timeString += hours + ' Giờ ';
+  }
+  if (minutes > 0) {
+    timeString += minutes + ' Phút';
+  }
+  return timeString.trim();
+}
+
+export function metersToKm(meters: any) {
+  const km = meters / 1000;
+  return Math.round(km * 10) / 10;
+}
+
+export function formatDistance(distance: number): string {
+  let formattedDistance = Math.round(distance / 100) * 100;
+  if (formattedDistance >= 1000) {
+    formattedDistance /= 1000;
+    return `${formattedDistance.toFixed(1)} km`;
+  }
+  return `${formattedDistance} m`;
+}
